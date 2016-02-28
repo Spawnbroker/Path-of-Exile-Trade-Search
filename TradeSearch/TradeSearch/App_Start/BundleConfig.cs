aa608cbefff0d1c23d70959852d6ddb681813cca
Jim Eiche <eiche_jim@yahoo.com>
2016-02-28 15:10:23 -0500

    Got basic OWIN authentication up and running.

--- TradeSearch/TradeSearch/App_Start/BundleConfig.cs
+++ TradeSearch/TradeSearch/App_Start/BundleConfig.cs
@@ -12,6 +12,12 @@ namespace TradeSearch
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkID=303951
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
             bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
                             "~/Scripts/WebForms/WebForms.js",
                             "~/Scripts/WebForms/WebUIValidation.js",
--- TradeSearch/TradeSearch/Global.asax.cs
+++ TradeSearch/TradeSearch/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Web.Mvc;
 
 namespace TradeSearch
 {
@@ -13,7 +14,8 @@ namespace TradeSearch
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
